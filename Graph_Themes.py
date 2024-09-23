ae6480cb15d9875eb028e87c3f69c02e2aaba7ad
sohqy <sohqiaoyan@gmail.com>
2024-09-23 11:12:23 +0100

    Adjusted theme

--- Graph_Themes.py
+++ Graph_Themes.py
@@ -2,8 +2,11 @@ import plotly.graph_objects as go
 import plotly.io as pio
 
 pio.templates["NZ_Calc"] = go.layout.Template(
-    layout=go.Layout(
-        font_family="Libre Franklin",
-        font_color="#0000cd",
-    )
+    layout={
+        'font': {'family': 'LibreFranklin',
+                 'size': 12,
+                 'color': "#0000cd",
+                 },
+        'colorway': ['#ec7424', '#a4abab'],
+    }
 )
