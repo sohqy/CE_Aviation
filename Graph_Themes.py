585f34c48c70e07f944b2677a4ca7ed7855ed28a
sohqy <sohqiaoyan@gmail.com>
2024-09-23 11:16:22 +0100

    theme

--- Graph_Themes.py
+++ Graph_Themes.py
@@ -3,10 +3,10 @@ import plotly.io as pio
 
 pio.templates["NZ_Calc"] = go.layout.Template(
     layout={
-        'font': {'family': 'LibreFranklin',
+        'font': {'family': 'libre-franklin',
                  'size': 12,
                  'color': "#0000cd",
                  },
-        'colorway': ['#ec7424', '#a4abab'],
+        'colorway': ['#fa8072', '#40e0d0', '#f0e68c'],
     }
 )
